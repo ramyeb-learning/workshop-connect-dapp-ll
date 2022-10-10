8bc33a5113e95e3cc570ce4221cde3f4e05a6c4c
RamyEB <ramy.elb@gmail.com>
2022-10-10 12:03:12 +0200

    step 3

--- src/api/useFetchMessages.ts
+++ src/api/useFetchMessages.ts
@@ -3,14 +3,19 @@ import { domain } from "../utils/EIP712";
 import { useEffect } from 'react';
 import { useContractRead } from "wagmi";
 
-/* STEP #1
-    TODO: Develop the logic to fetch the last 10 messages
-    TIPS: There is a hook from the wagmi documentation that allows you to call a read-only method
-    Link of the documentation: https://wagmi.sh/docs/getting-started
-*/
 const useFetchMessages = () => {
-  // Connect the variables below
-  return { data: [], isError: null, isLoading: false };
+  const { data, isError, isLoading, refetch } = useContractRead({
+    addressOrName: domain.verifyingContract,
+    contractInterface: artifacts.abi,
+    functionName: 'getLast10Messages',
+  })
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  return { data, isError, isLoading };
+
 };
 
 export default useFetchMessages;
--- src/api/usePostMessage.ts
+++ src/api/usePostMessage.ts
@@ -1,7 +1,11 @@
 import artifacts from "../utils/contract.json";
 import { domain, types } from "../utils/EIP712";
+import { useSignTypedData, useContractWrite } from 'wagmi'
+
 
 const WRITE_ASYNC_OVERRIDES_GASLIMIT = { gasLimit: 250_000 };
+
+
 
 /* STEP #2
     TODO: Develop the logic to sign typed data with connected account
@@ -9,16 +13,34 @@ const WRITE_ASYNC_OVERRIDES_GASLIMIT = { gasLimit: 250_000 };
     Link of the documentation: https://wagmi.sh/docs/getting-started
 */
 
+
 /* STEP #3
     TODO: Develop the logic to post a message
     TIPS: There is a hook from the wagmi documentation that allows you to call a write method
     Link of the documentation: https://wagmi.sh/docs/getting-started
 */
-const usePostMessage = () => {
-  // Implement the logic to sign typed data with connected account
 
-  const postMessage = (message: string, author: string) => {
-    // Implement me :)
+const usePostMessage = () => {
+  
+  
+  const { data, isLoading, isSuccess, write, writeAsync } = useContractWrite({
+    mode: "recklesslyUnprepared", 
+    addressOrName: domain.verifyingContract,
+    contractInterface: artifacts.abi,
+    functionName: 'sendMessage',
+})
+
+  const { isError: is712Error, isSuccess: is712Success, error: error712, signTypedDataAsync } = useSignTypedData();
+  
+  const postMessage = async (message: string, author: string) => {
+    try {
+      const value = { contents: message, from: author };
+      const signature = await signTypedDataAsync({ value, domain, types });
+      const tx = await writeAsync({ recklesslySetUnpreparedArgs: [message, signature], recklesslySetUnpreparedOverrides: WRITE_ASYNC_OVERRIDES_GASLIMIT });
+      console.log(`tx hash: ${tx.hash}`);
+    } catch (e) {
+      throw new Error(e);
+    }
   };
 
   return {
