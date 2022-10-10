04056e5565021f2017cb9e8428885fe9eeb1026a
RamyEB <ramy.elb@gmail.com>
2022-10-10 13:45:24 +0200

    step 5

--- src/api/useLikeMessage.ts
+++ src/api/useLikeMessage.ts
@@ -1,3 +1,4 @@
+import { useSignMessage, useContractWrite } from 'wagmi'
 import { domain } from "../utils/EIP712";
 import artifacts from "../utils/contract.json";
 
@@ -5,27 +6,42 @@ const WRITE_ASYNC_OVERRIDES_GASLIMIT = { gasLimit: 250_000 };
 
 /* STEP #4
     TODO: Develop the logic to sign a message
-    TIPS: There is a hook from the wagmi documentation that allows you to sign message (not a typed one)
+    TIP: There is a hook from the Wagmi documentation to sign message (not typed ones)
     Link of the documentation: https://wagmi.sh/docs/getting-started
 */
 
 /* STEP #5
     TODO: Develop the logic to like a message
-    TIPS: There is a hook from the wagmi documentation that allows you to call a write method
+    TIP: There is a hook from the Wagmi documentation to call the write method
     Link of the documentation: https://wagmi.sh/docs/getting-started
 */
 const useLikeMessage = () => {
+  const { data, isError, isLoading, isSuccess, error, writeAsync } = useContractWrite({
+    addressOrName: domain.verifyingContract,
+    contractInterface: artifacts.abi,
+    functionName: 'likeMessage',
+  });
+  const { error: erroSign, isError: isSignError, isLoading: isSignLoading, isSuccess: isSignSuccess, signMessageAsync } = useSignMessage();
+
+
   const likeMessage = async (id: string, author: string) => {
-    // Implement me :)
+    try {
+      const signature = await signMessageAsync({ message: `I like the post #${id} posted by ${author}` });
+      console.log({ signature })
+      const tx = await writeAsync({ args: [id], overrides: WRITE_ASYNC_OVERRIDES_GASLIMIT, });
+      console.log(`tx hash: ${tx.hash}`);
+    } catch (e) {
+      throw new Error(e);
+    }
   };
 
   return {
     likeMessage,
-    data: null,
-    error: null,
-    isError: false,
-    isLoading: false,
-    isSuccess: false,
+    data,
+    error: error || erroSign,
+    isError: isError || isSignError,
+    isLoading: isLoading || isSignLoading,
+    isSuccess: isSuccess && isSignSuccess
   };
 };
 
